ca3fece42a0c7b41e66002c44e1f2af30d2b7024
dehowef <dehowef@pc28.cs.ucdavis.edu>
2015-11-16 20:58:04 -0800

    started split-if

--- hw4.lisp
+++ hw4.lisp
@@ -7,7 +7,8 @@
 	(cond
 		((null l) 0)
 		((listp (car l)) (+ (all-length(car l)) (all-length(cdr l))) ) ; (+ (+ 1 (all-length(cdr l)))) ) 
-		(t (+ 1 (all-length(cdr l)))))
+		(t (+ 1 (all-length(cdr l))))
+	)
 
 )
 
@@ -18,7 +19,17 @@
 (defun before(a b l)
 )
 
-(defun split-if(fun l)
+(defun split-if (f l)
+	(mapcar f l)
+	(list splitLeft f l splitRight f l ) ;left will have t, right will have nil
+)	
+
+(splitLeft(f l)
+	(mapcar f l) 	
+)
+
+(splitRight(f l)
+	(mapcar f l)
 )
 
 (defun group(l n)
