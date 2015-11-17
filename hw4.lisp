bbbd74216e2b9ee3bad8c9c0b3e57013201affd5
stcalica <kycalica@gmail.com>
2015-11-16 22:26:33 -0800

    mostn

--- hw4.lisp
+++ hw4.lisp
@@ -2,7 +2,6 @@
 ;;; Your solution goes in this file ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
- 
 (defun all-length (l)
 	(cond
 		((null l) 0)
@@ -25,7 +24,7 @@
 (defun before (a b l) 
         (cond
                 ((null l) nil)
-                ((equal a (car l) ) (cons (car l) (after_a b (cdr l))))
+                ((equal a (car l) ) cons (car l) (after_a b (cdr l)))
                 (t (before a b (cdr l)) )
         )
 
@@ -36,5 +35,20 @@
 (defun group(l n)
 )
 
+
 (defun mostn(f l)
+
+	(setq l (mapcar #'f l))
+	(setq m (max l) )
+	(mostn_help m f l)
+
+)
+
+(defun mostn_help(m f l) 
+	(cond
+		((null l) nil) 
+		((equal m  (funcall f (car l) ) )   cons( (car l) (mostn_help m f (cdr l))) )
+		(t  (mostn_help m f (cdr l)) ) 
+	)
+
 )
